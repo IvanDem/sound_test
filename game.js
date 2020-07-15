5645de676210c464f966dc462d1edb5d9db4db42
IvanDem <bahko@mail.ru>
2020-07-15 15:30:59 +0200

    renamed sounds

--- game.js
+++ game.js
@@ -27,35 +27,28 @@ class gameScene extends Phaser.Scene {
     super("gameScene");
   }
 
-  preload() {
-    // this.load.audio("menu_music", ["audio/MUS01.ogg","audio/MUS01.mp3"]);
-  }
-
   create() {
     let self = this
     let soundNumber = 0
     let scriptTimer = this.time.addEvent({
       delay: 1000,
       callback: () => {
-        // self.playNextSound(soundNumber)
-        // self.counter.setText("sound number " + soundNumber)
-        // soundNumber++
+        self.playNextSound(soundNumber)
+        self.counter.setText("sound number " + soundNumber)
+        soundNumber++
       },
       repeat: -1
     })
 
-    this.counter = this.add.text(100, 100, 'Phaser');
+    this.counter = this.add.text(100, 100, "sound number " + soundNumber);
     this.audio = false
   }
 
   playNextSound(soundNumber) {
     console.log("playing sound " + soundNumber);
     let self = this
-    if (soundNumber > 800) {
-      return
-    }
 
-    this.load.audio("audio" + soundNumber, ["audio/goat.ogg", "audio/goat.mp3"]);
+    this.load.audio("audio" + soundNumber, ["audio/music.ogg", "audio/music.mp3"]);
     this.load.start();
     this.load.once('complete', function() {
       if (self.audio) {
